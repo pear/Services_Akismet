c0d4f67dc8b54f1369b8c4cdaccd198c688ede1f
Daniel O'Connor <clockwerx@php.net>
2010-02-11 23:08:31 +0000

    Fixed AllTests.php
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Akismet/trunk@294919 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -51,7 +51,6 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Services_Akismet_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
 require_once 'SocketsTest.php';
